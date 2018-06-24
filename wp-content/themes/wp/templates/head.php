202863044772769ca505c3b5a8fec57c77a0b898
Jasen Wyatt <jasen.wyatt@gmail.com>
2018-06-24 14:57:16 -0400

    rem humans; updates htaccess;

--- wp-content/themes/wp/templates/head.php
+++ wp-content/themes/wp/templates/head.php
@@ -2,6 +2,5 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="author" href="humans.txt" />
   <?php wp_head(); ?>
 </head>
