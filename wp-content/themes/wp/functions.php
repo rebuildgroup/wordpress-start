5056e4e64863fba25254aed2497f2cd6728c3e2e
Jasen Wyatt <jasen.wyatt@gmail.com>
2018-07-14 21:15:05 -0400

    adds navwalker option;

--- wp-content/themes/wp/functions.php
+++ wp-content/themes/wp/functions.php
@@ -10,13 +10,14 @@
  * @link https://github.com/roots/sage/pull/1042
  */
 $sage_includes = [
-  'lib/assets.php',       // Scripts and stylesheets
-  'lib/extras.php',       // Custom functions
-  'lib/setup.php',        // Theme setup
-  'lib/titles.php',       // Page titles
-  'lib/wrapper.php',      // Theme wrapper class
-  'lib/customizer.php',   // Theme customizer
-  'lib/head-cleaner.php'  // Removes Junk from head
+  'lib/assets.php',         // Scripts and stylesheets
+  'lib/extras.php',         // Custom functions
+  'lib/setup.php',          // Theme setup
+  'lib/titles.php',         // Page titles
+  'lib/wrapper.php',        // Theme wrapper class
+  'lib/customizer.php',     // Theme customizer
+  'lib/head-cleaner.php',   // Removes Junk from head
+  //'lib/wp-navwalker.php'    // WP Bootstrap Navwalker
 ];
 
 foreach ($sage_includes as $file) {
