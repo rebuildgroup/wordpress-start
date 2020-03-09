a4d61de273cb62d2143216a93a5d7b6a870e6dc1
Keith Walker <developer@MacBook-Air.localdomain>
2020-03-09 10:32:47 -0400

    Added: Beaver Builder Color, Hamburger CSS & JS, as well as updated Nav Walker

---
+++ wp-content/themes/wp/assets/scripts/lib/hamburger.js
@@ -0,0 +1,11 @@
+(function($) {
+    $(document).ready(function() {
+      var $hamburger = $(".hamburger");
+  
+      $hamburger.on("click", function(e) {
+          $hamburger.toggleClass("is-active");
+      // Do something else, like open/close menu
+      });
+    });
+  })(jQuery);
+  
--- wp-content/themes/wp/lib/wp-navwalker.php
+++ wp-content/themes/wp/lib/wp-navwalker.php
@@ -182,8 +182,19 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) {
 			$atts['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
 			// If item has_children add atts to <a>.
 			if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && $args->depth > 1 ) {
-				$atts['href']          = '#';
-				$atts['data-toggle']   = 'dropdown';
+				//$atts['href']          = '#';
+				//$atts['data-toggle']   = 'dropdown';
+				if ( wp_is_mobile() ) {
+			      // Parent link inactiv
+			      $atts['href'] = '#';
+			      // Parent link toggles dropdown
+			      $atts['data-toggle'] = 'dropdown';
+			  } else {
+			      // Parent link active
+			    $atts['href'] = ! empty( $item->url ) ? $item->url : '';
+			    // Parent link shows dropdown on hover
+			    $atts['data-hover'] = 'dropdown';
+			  }
 				$atts['aria-haspopup'] = 'true';
 				$atts['aria-expanded'] = 'false';
 				$atts['class']         = 'dropdown-toggle nav-link';
